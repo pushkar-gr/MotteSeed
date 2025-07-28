96ab242ae0281baeb54464b8363fa6d87488f35f
pushkar-gr <gr.pushkar.7@gmail.com>
2025-07-28 09:44:57 +0530

    add: deserialize method for message

--- src/core/peer/message.rs
+++ src/core/peer/message.rs
@@ -1,4 +1,6 @@
 use bytes::{BufMut, Bytes, BytesMut};
+use std::io;
+use thiserror::Error;
 
 //message enum to represent message tyes in BitTorrent protocol
 #[derive(Debug)]
@@ -103,4 +105,112 @@ impl Message {
 
         buf.freeze()
     }
+
+    //deserialize bytes to Message
+    pub fn deserialize(bytes: &[u8]) -> Result<Self, MessageError> {
+        //min 4 bytes for message length
+        if bytes.len() < 4 {
+            return Err(MessageError::MessageTooShort);
+        }
+
+        //get message length
+        let len = u32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
+
+        if len == 0 {
+            return Ok(Message::KeepAlive);
+        }
+
+        //check if message has message id
+        if bytes.len() < 5 {
+            return Err(MessageError::MessageTooShort);
+        }
+
+        //get message id
+        let msg_id = bytes[4];
+
+        match msg_id {
+            0 => Ok(Message::Choke),
+            1 => Ok(Message::UnChoke),
+            2 => Ok(Message::Interested),
+            3 => Ok(Message::NotInterested),
+            4 => {
+                if bytes.len() != 9 {
+                    return Err(MessageError::MessageTooShort);
+                }
+                let piece_index = u32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
+                Ok(Message::Have(piece_index))
+            }
+            5 => {
+                //remove len 1 to get X
+                let len = len - 1;
+                if bytes.len() < 5 + len as usize {
+                    return Err(MessageError::MessageTooShort);
+                }
+                Ok(Message::Bitfield(Bytes::copy_from_slice(
+                    &bytes[5..(5 + len as usize)],
+                )))
+            }
+            6 => {
+                if bytes.len() != 17 {
+                    return Err(MessageError::MessageTooShort);
+                }
+                let index = u32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
+                let begin = u32::from_be_bytes([bytes[9], bytes[10], bytes[11], bytes[12]]);
+                let length = u32::from_be_bytes([bytes[13], bytes[14], bytes[15], bytes[16]]);
+                Ok(Message::Request {
+                    index,
+                    begin,
+                    length,
+                })
+            }
+            7 => {
+                //remove len 9 to get X
+                let len = len - 9;
+                if bytes.len() < 13 + len as usize {
+                    return Err(MessageError::MessageTooShort);
+                }
+                let index = u32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
+                let begin = u32::from_be_bytes([bytes[9], bytes[10], bytes[11], bytes[12]]);
+                let block = Bytes::copy_from_slice(&bytes[13..(13 + len as usize)]);
+                Ok(Message::Piece {
+                    index,
+                    begin,
+                    block,
+                })
+            }
+            8 => {
+                if bytes.len() != 17 {
+                    return Err(MessageError::MessageTooShort);
+                }
+                let index = u32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
+                let begin = u32::from_be_bytes([bytes[9], bytes[10], bytes[11], bytes[12]]);
+                let length = u32::from_be_bytes([bytes[13], bytes[14], bytes[15], bytes[16]]);
+                Ok(Message::Cancel {
+                    index,
+                    begin,
+                    length,
+                })
+            }
+            9 => {
+                if bytes.len() != 7 {
+                    return Err(MessageError::MessageTooShort);
+                }
+                let port = u16::from_be_bytes([bytes[5], bytes[6]]);
+                Ok(Message::Port(port))
+            }
+            i => Err(MessageError::InvalidType(i)),
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum MessageError {
+    #[error("IO Error: {0}")]
+    IO(#[from] io::Error),
+
+    #[error("Invalid message type: {0}")]
+    InvalidType(u8),
+
+    #[error("Message too short")]
+    MessageTooShort,
 }

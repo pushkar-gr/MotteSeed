39be4379aff28275169d47c25adbbe3eebca4b7a
pushkar-gr <gr.pushkar.7@gmail.com>
2025-08-07 11:38:25 +0530

    add: methods to block

--- src/core/peer/piece/block.rs
+++ src/core/peer/piece/block.rs
@@ -3,15 +3,15 @@ use std::time::Instant;
 
 //structure to represent a block
 #[derive(Debug)]
-pub struct Block {
-    pub index: u32,          //index of the piece block belongs to
-    pub offset: u32,         //offset of block in the piece
-    pub length: u32,         //lenght of data
-    pub data: Option<Bytes>, //data store in bytes
-    pub state: BlockState,   //block state
+pub struct Block<'a> {
+    pub index: u32,            //index of the piece block belongs to
+    pub offset: u32,           //offset of block in the piece
+    pub length: u32,           //lenght of data
+    pub data: Option<Bytes>,   //data store in bytes
+    pub state: BlockState<'a>, //block state
 }
 
-impl Block {
+impl<'a> Block<'a> {
     pub fn new(index: u32, offset: u32, length: u32) -> Self {
         Self {
             index,
@@ -21,13 +21,27 @@ impl Block {
             state: BlockState::Missing,
         }
     }
+
+    pub fn isComplete(&self) -> bool {
+        matches!(self.state, BlockState::Received | BlockState::Written)
+    }
+
+    pub fn request_from_peer(&mut self, peer_ip: &'a [u8; 6]) {
+        self.state = BlockState::Requested {
+            peer_ip,
+            instant: Instant::now(),
+        }
+    }
 }
 
 //represents block state
 #[derive(Debug)]
-pub enum BlockState {
-    Missing,                                          //block needs to be downloaded
-    Requested { peer_ip: [u8; 6], instant: Instant }, //block requested from a peer
-    Received,                                         //block received from a peer
-    Written,                                          //block written to disk
+pub enum BlockState<'a> {
+    Missing, //block needs to be downloaded
+    Requested {
+        peer_ip: &'a [u8; 6],
+        instant: Instant,
+    }, //block requested from a peer
+    Received, //block received from a peer
+    Written, //block written to disk
 }

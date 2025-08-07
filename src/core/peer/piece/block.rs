e70b9495da887e7780441e6aa851b576c690017a
pushkar-gr <gr.pushkar.7@gmail.com>
2025-08-07 11:10:10 +0530

    add: basic block strucutre

---
+++ src/core/peer/piece/block.rs
@@ -0,0 +1,21 @@
+use bytes::Bytes;
+use std::time::Instant;
+
+//structure to represent a block
+#[derive(Debug)]
+pub struct Block {
+    pub index: u32,        //index of the piece block belongs to
+    pub offset: u32,       //offset of block in the piece
+    pub length: u32,       //lenght of data
+    pub data: Bytes,       //data store in bytes
+    pub state: BlockState, //block state
+}
+
+//represents block state
+#[derive(Debug)]
+pub enum BlockState {
+    Missing,                                          //block needs to be downloaded
+    Requested { peer_ip: [u8; 6], instant: Instant }, //block requested from a peer
+    Received,                                         //block received from a peer
+    Written,                                          //block written to disk
+}
